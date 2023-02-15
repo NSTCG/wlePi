b5cb4c070c5f329ee146a2f9e57431f5207415ea
NSTCG <nithinsteven32@gmail.com>
2023-02-16 00:21:13 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+/**
+ * /!\ This file is auto-generated.
+ *
+ * This is the entry point of your standalone application.
+ *
+ * You should register the component you need here, e.g.,
+ *
+ * ```
+ * import { MyComponent } from './my-component.js';
+ *
+ * WL.registerComponent(MyComponent);
+ * ```
+ */
+import '@wonderlandengine/components';
+import './button.js';
