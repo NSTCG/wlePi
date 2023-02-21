b12485abf6dcba84800231ede2306ebd4999cd4e
nstcg <nithinsteven32@gmail.com>
2023-02-21 23:46:34 +0530

    add startup.sh

---
+++ startup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# change to the directory where the Git repository is located
+cd /home/nstcg/Desktop/SwitchAutomation/wlePi
+
+# fetch the latest changes from the remote repository
+git fetch --all --force
+
+
+# change to the directory where the Node.js application is located
+cd /home/nstcg/Desktop/SwitchAutomation/wlePi/NodeJSApp
+
+# install any new dependencies from npm
+npm install
+
+# run the Node.js application with the app.js file
+node app.js
